9bf41cda05a3476442d49ef7830a119877ae54a8
windyAnn <luoan99@gmail.com>
2016-08-03 08:14:46 +0800

    'myshow'

--- myshow/js/change.js
+++ myshow/js/change.js
@@ -1,28 +1,25 @@
-
-
 $(document).ready(function () {
 	var lx = [];
 	var ly = [];
 	var rot = [];
 	var zIndex = 1;
 	$("img").each(function (i, element) {
-
-	  if(i<6){
-		lx[i] = parseInt(Math.random() * 50 + 160 * i);
-		ly[i] = parseInt(Math.random() * 5  + 20);
-		rot[i] = parseInt(Math.random() * 35);
-	  }
-	  else {
-		lx[i] = parseInt(Math.random() * 60 + 180* (i-6));
-		ly[i] = parseInt(Math.random() * 100  + 260);
-		rot[i] = parseInt(Math.random() * (-35));
-	  }
-	  
+		if (i < 6) {
+			lx[i] = parseInt(Math.random() * 50 + 160 * i);
+			ly[i] = parseInt(Math.random() * 5 + 20);
+			rot[i] = parseInt(Math.random() * 35);
+		}
+		else {
+			lx[i] = parseInt(Math.random() * 60 + 180 * (i - 6));
+			ly[i] = parseInt(Math.random() * 100 + 260);
+			rot[i] = parseInt(Math.random() * (-35));
+		}
 		$(element).css({
 			left: lx[i],
 			top: ly[i],
 			transform: "rotate(" + rot[i] + "deg)"
-		}).bind("mouseover", function () {
+		});
+		$(element).bind("mouseover", function () {
 			zIndex++;
 			$(element).css({
 				"z-index": zIndex,
